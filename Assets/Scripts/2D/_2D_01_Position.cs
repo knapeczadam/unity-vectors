cca0eee9581c2d6eb41339f27efcf6adc4a72d7d
Knapecz Ádám <knapecz.adam@gmail.com>
2017-11-28 00:06:10 +0100

    2D Position

---
+++ Assets/Scripts/2D/_2D_01_Position.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class _2D_01_Position : MonoBehaviour
+{
+
+	[Header("Player")] 
+	private GameObject _player;
+	public float player_x;
+	public float player_y;
+
+	private void OnEnable()
+	{
+		_player = GameObject.FindWithTag(Constant.PLAYER_2D);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		/*
+		 * Q: Should I use Translate instead of position?
+		 * _player.transform.Translate(new Vector3(player_x, 0, player_y));
+		 *
+		 * Q: What's the meaning of using Time.deltaTime?
+		 * _player.transform.Translate(new Vector3(player_x, 0, player_y) * Time.deltaTime);
+		 */
+		_player.transform.position = new Vector3(player_x, 0, player_y);
+
+	}
+}
---
+++ Assets/Scripts/Constant.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constant
+{
+	public static readonly string PLAYER_2D = "Player2D";
+	public static readonly string PLAYER_3D = "Player3D";
+	public static readonly string ENEMY_2D = "Enemy2D";
+	public static readonly string ENEMY_3D = "Enemy3D";
+}
