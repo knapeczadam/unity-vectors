537c994bc6673ac92ce7a2b44bc6725772860569
Knapecz Ádám <knapecz.adam@gmail.com>
2018-01-13 16:10:02 +0100

    2D Direction

---
+++ Assets/Scripts/2D/_2D_00_Direction.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+
+[ExecuteInEditMode]
+public class _2D_00_Direction : MonoBehaviour
+{
+    [ReadOnly]
+    [Header("")]
+    public Vector2 Direction;
+
+    [Header("")] 
+    public bool Zero;
+    public bool One;
+    public bool Up;
+    public bool Down;
+    public bool Left;
+    public bool Right;
+
+    // Can you setup these fields like the ones you see above (Zero, One, Up, etc.) ?
+    public bool PositiveInfinity;
+    public bool NegativeInfitiny;
+
+    private BoolWrapper _zero;
+    private BoolWrapper _one;
+    private BoolWrapper _up;
+    private BoolWrapper _down;
+    private BoolWrapper _left;
+    private BoolWrapper _right;
+
+    private BoolWrapper[] _dirs;
+    private int _current = -1;
+
+    private class BoolWrapper
+    {
+        /*
+         * Q: What is AutoProperty in C#?
+         */
+        public bool Value { get; set; }
+
+        public BoolWrapper(bool value)
+        {
+            this.Value = value;
+        }
+    }
+
+    private void OnEnable()
+    {
+        InitWrappers();
+        _dirs = new[]
+        {
+            _zero,
+            _one,
+            _up,
+            _down,
+            _left,
+            _right,
+        };
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateWrappers();
+        SetCurrent();
+        Direction = UpdateDirection();
+        Draw();
+    }
+
+    private void InitWrappers()
+    {
+        _zero = new BoolWrapper(false);
+        _one = new BoolWrapper(false);
+        _up = new BoolWrapper(false);
+        _down = new BoolWrapper(false);
+        _left = new BoolWrapper(false);
+        _right = new BoolWrapper(false);
+    }
+
+    private void UpdateWrappers()
+    {
+        _zero.Value = Zero;
+        _one.Value = One;
+        _up.Value = Up;
+        _down.Value = Down;
+        _left.Value = Left;
+        _right.Value = Right;
+    }
+
+    private void SetCurrent()
+    {
+        bool ticked = false;
+        for (int i = 0; i < _dirs.Length; i++)
+        {
+            if (_current == -1 && _dirs[i].Value)
+            {
+                _current = i;
+            }
+            if (_current >= 0 && _dirs[i].Value && i != _current)
+            {
+                _dirs[_current].Value = false;
+                _current = i;
+            }
+            if (_dirs[i].Value)
+            {
+                ticked = true;
+            }
+        }
+        _current = ticked ? _current : -1;
+
+        if (_current != -1)
+        {
+            for (int j = 0; j < _dirs.Length; j++)
+            {
+                if (j != _current)
+                {
+                    switch (j)
+                    {
+                        case 0:
+                            Zero = false;
+                            break;
+                        case 1:
+                            One = false;
+                            break;
+                        case 2:
+                            Up = false;
+                            break;
+                        case 3:
+                            Down = false;
+                            break;
+                        case 4:
+                            Left = false;
+                            break;
+                        case 5:
+                            Right = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private Vector2 UpdateDirection()
+    {
+        Vector2 dir = new Vector2();
+        if (Zero)
+        {
+            dir = Vector2.zero;
+        }
+        else if (One)
+        {
+            dir = Vector2.one;
+        }
+        else if (Up)
+        {
+            dir = Vector2.up;
+        }
+        else if (Down)
+        {
+            dir = Vector2.down;
+        }
+        else if (Left)
+        {
+            dir = Vector2.left;
+        }
+        else if (Right)
+        {
+            dir = Vector2.right;
+        }
+
+        return dir;
+    }
+
+    private void Draw()
+    {
+        Debug.DrawLine(Vector2.zero, Direction, Color.cyan);
+    }
+}
---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/*
+ * https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+ */
+public class ReadOnlyAttribute : PropertyAttribute
+{
+ 
+}
